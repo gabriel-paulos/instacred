ee1e9a538017427e2ce5456e19d1c549565877af
gabriel-paulos <57543991+gabriel-paulos@users.noreply.github.com>
2021-09-17 20:59:31 -0400

    Data models

---
+++ Model/personShown.swift
@@ -0,0 +1,22 @@
+//
+//  personShown.swift
+//  instacred(SwiftUI)
+//
+//  Created by Maria Paulo on 2021-09-15.
+//
+//
+//import SwiftUI
+//import RealmSwift
+//
+//class PersonShown: Object, Identifiable{
+//    
+//    @Persisted var name: String
+//    @Persisted var post: Post
+//    
+//    convenience init(name:String, post: Post){
+//        self.init()
+//        self.name = name
+//        self.post = post
+//    }
+//    
+//}
